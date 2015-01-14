a0791231d9060408246f4a64336e91db028ce828
floringrigoriu <Xfree86_585>
2015-01-14 11:02:15 -0800

    test 1

---
+++ ExcelAddIn1/ExcelAddIn1/Ribbon1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Office.Tools.Ribbon;
+using System.Windows.Forms;
+
+namespace ExcelAddIn1
+{
+    public partial class Ribbon1
+    {
+        private void Ribbon1_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+
+        private void helloButton_Click(object sender, RibbonControlEventArgs e)
+        {
+            MessageBox.Show("Hello, world of Ads");
+        }
+
+        private void button1_Click(object sender, RibbonControlEventArgs e)
+        {
+            MessageBox.Show("Hello, world of Ads, v2");
+        }
+    }
+}
