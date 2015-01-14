d6d872a32634b1bde5a3fe39c7e2f91ab2405e52
floringrigoriu <Xfree86_585>
2015-01-14 13:37:19 -0800

    Add unit test AdAccount

---
+++ ExcelAddIn1/GraphCallerTest/AdAccountTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GraphCaller;
+
+namespace GraphCallerTest
+{
+    [TestClass]
+    public class AdAccountTest
+    {
+        private string testAdAccount = "abc";//do update with the real deal
+        private const int florinAdAccount = 123 ;  
+
+        [TestMethod]
+        public void GetAdAccount()
+        {
+            // call
+            var account = AdAccount.getAdAccount(testAdAccount, florinAdAccount);
+            // assert
+            Assert.AreNotEqual(null, account);
+        }
+    }
+}
