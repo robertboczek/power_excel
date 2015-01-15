448cefa5c76a654eec87a4e3892176c35934d317
floringrigoriu <Xfree86_585>
2015-01-14 18:09:26 -0800

    Add Post stuff

--- ExcelAddIn1/GraphCallerTest/AdAccountTest.cs
+++ ExcelAddIn1/GraphCallerTest/AdAccountTest.cs
@@ -7,6 +7,7 @@ namespace GraphCallerTest
     [TestClass]
     public class AdAccountTest
     {
+        // This is an old invalidate valude , but should serve as an warning no leakage of sensitive and sensible data
         public static readonly string testAdAccount = "CAACZBwbJIzL0BAPXZC7kufhmUsbBgaB8A8ZCh9SEQx6Ms4BpNCo1xPlvdJHjG263qCJRNK7Moa15zpPQCpdxMrxOHCb4aZB5zDSQBLpjsUWKfZCBZCH80cpZBUDkZCQlqoZAT7v4TFKAOQbFPnKWezMe3tMQnhYZCTQePLL7FJVRexAmkxSJFtU8kO";
             //do update with the real deal - access token
         public const long florinAdAccount = 10151318637546538;  
